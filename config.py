ed54710b30fe5fc08ca4cf3ef8431941fc48c210
ocejian <zrestuprayitno@gmail.com>
2025-07-29 06:54:17 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 27365066  # Ganti dengan API ID kamu
+API_HASH = "fc27b1d891241e20985b6b591525e8ee"
+BOT_TOKEN = "7565141853:AAFTCEWBfqavhu3RpQ63KhtIVpLvmtgyvbE"
+OWNER_ID = 8404394288  # ID Telegram admin bot utama
+
+CHANNEL_ID = -1002630667822  # ID channel menfes
+MODERATION = False  # True = moderasi on, False = auto post
+LOG_CHAT_ID = -1002870766308
