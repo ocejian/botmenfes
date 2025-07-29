a6198f054f6607444726a86285edbf0c9c05fccb
ocejian <zrestuprayitno@gmail.com>
2025-07-29 07:10:31 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-    @app.on_message(filters.command("start") & filters.private)
-    async def start_handler(_, message: Message):
-        await message.reply(
-            f"👋 Halo {message.from_user.mention},\nSelamat datang di bot Menfes!",
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("📮 Kirim Menfes", switch_inline_query_current_chat="")],
-                [InlineKeyboardButton("📌 Support", url="https://t.me/ofckelrapillas")],
-            ])
-        )
+@app.on_message(filters.command("start") & filters.private)
+async def start_handler(_, message: Message):
+    await message.reply(
+        f"👋 Halo {message.from_user.mention},\nSelamat datang di bot Menfes!",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("📮 Kirim Menfes", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton("📌 Support", url="https://t.me/ofckelrapillas")],
+        ])
+    )
