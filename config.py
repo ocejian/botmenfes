894b69fe23853d24a46f324d9743b4a18934f8c8
ocejian <zrestuprayitno@gmail.com>
2025-08-07 10:58:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 27365066  # Ganti dengan API ID kamu
 API_HASH = "fc27b1d891241e20985b6b591525e8ee"
-BOT_TOKEN = "7565141853:AAFTCEWBfqavhu3RpQ63KhtIVpLvmtgyvbE"
-OWNER_ID = 8404394288  # ID Telegram admin bot utama
+BOT_TOKEN = "7558192384:AAEtOWpZiZbaS-WXYPChzxBZKZc8214thpk"
+OWNER_ID = 827203629  # ID Telegram admin bot utama
 
 CHANNEL_ID = -1002630667822  # ID channel menfes
 MODERATION = False  # True = moderasi on, False = auto post
